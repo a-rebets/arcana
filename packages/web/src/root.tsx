eeabd8b41d0ef41ec69c9946896d73a2a7f51eea
Artem Popelyshev <apopelyshev@gmail.com>
2025-09-30 15:16:10 +0200

    fix: links in the head

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -3,7 +3,15 @@ import { ConvexQueryClient } from "@convex-dev/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "next-themes";
-import { Outlet, Scripts, ScrollRestoration, useMatches } from "react-router";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useMatches,
+} from "react-router";
+import type { Route } from "./+types/root";
 import { Toaster } from "./components/ui/sonner";
 import { cn } from "./lib/utils";
 
@@ -21,6 +29,23 @@ const queryClient = new QueryClient({
 });
 convexQueryClient.connect(queryClient);
 
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    href: "/favicon-16x16.png",
+    sizes: "16x16",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-32x32.png",
+    sizes: "32x32",
+    type: "image/png",
+  },
+  { rel: "apple-touch-icon", href: "/apple-touch-icon.png", sizes: "180x180" },
+  { rel: "icon", href: "/favicon.ico" },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   const matches = useMatches();
   const currentRoute = matches[matches.length - 1] as {
@@ -32,25 +57,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Arcana</title>
+        <Meta />
+        <Links />
       </head>
       <body
         className={cn(

8d7dc1a25569530b98ab3de964a1690e7f5c5aba
Artem Popelyshev <apopelyshev@gmail.com>
2025-10-28 00:58:14 +0100

    fix: dynamic viewport for pop up keyboard

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -44,7 +44,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+        />
         <title>Arcana</title>
         <Meta />
         <Links />
